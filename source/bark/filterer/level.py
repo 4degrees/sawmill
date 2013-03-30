764b90c8de12e8e3e9e46ded75c20c58cfab09f9
Martin Pengelly-Phillips <dev@thesociable.net>
2013-03-30 22:06:14 +0000

    Allow levels to be set on Level filterer to avoid embedded global reference.

--- source/bark/filterer/level.py
+++ source/bark/filterer/level.py
@@ -9,12 +9,11 @@ from .base import Filterer
 class Level(Filterer):
     '''Filter logs according to defined log level limits.'''
 
-    def __init__(self, min='info', max=None):
+    def __init__(self, min='info', max=None, levels=bark.levels):
         '''Initialise filterer with *min* and *max* levels.
 
-        The values must be taken from the configured :py:attr:`bark.levels`
-        array. If the value given is None then it will be as if there
-        is not limit.
+        The values must be taken from passed *levels* array. If the value given
+        is None then it will be as if there is not limit.
 
         A :py:class:`~bark.log.Log` level value must fall between (inclusive)
         the *min* and *max* values.
@@ -24,6 +23,7 @@ class Level(Filterer):
         super(Level, self).__init__()
         self.min = min
         self.max = max
+        self.levels = levels
 
     def filter(self, log):
         '''Filter *log* if its level is not between defined range.
@@ -43,18 +43,18 @@ class Level(Filterer):
 
         # If level value not recognised, play safe and pass filter.
         try:
-            level_index = bark.levels.index(level)
+            level_index = self.levels.index(level)
         except ValueError:
             return False
 
         # Check against defined range.
         if self.min is not None:
-            min_index = bark.levels.index(self.min)
+            min_index = self.levels.index(self.min)
             if level_index < min_index:
                 return True
 
         if self.max is not None:
-            max_index = bark.levels.index(self.max)
+            max_index = self.levels.index(self.max)
             if level_index > max_index:
                 return True
 
--- test/unit/filterer/test_level.py
+++ test/unit/filterer/test_level.py
@@ -8,53 +8,63 @@ from bark.log import Log
 from bark.filterer.level import Level
 
 
-def test_no_level_present():
+def pytest_funcarg__levels(request):
+    '''Return levels.'''
+    return [
+        'debug',
+        'info',
+        'warning',
+        'error'
+    ]
+
+
+def test_no_level_present(levels):
     '''Test log record with no level information passes.'''
     log = Log()
-    filterer = Level()
+    filterer = Level(levels=levels)
     assert filterer.filter(log) is False
 
 
-def test_invalid_level():
+def test_invalid_level(levels):
     '''Test invalid level on log passes filter.'''
     log = Log(level='invalid_level')
-    filterer = Level()
+    filterer = Level(levels=levels)
     assert filterer.filter(log) is False
 
 
-def test_invalid_min_level():
+def test_invalid_min_level(levels):
     '''Test invalid min level on filterer raises ValueError.'''
     log = Log(level='info')
-    filterer = Level(min='invalid_level')
+    filterer = Level(min='invalid_level', levels=levels)
     with pytest.raises(ValueError):
         filterer.filter(log)
 
 
-def test_invalid_max_level():
+def test_invalid_max_level(levels):
     '''Test invalid max level on filterer raises ValueError.'''
     log = Log(level='info')
-    filterer = Level(max='invalid_level')
+    filterer = Level(max='invalid_level', levels=levels)
     with pytest.raises(ValueError):
         filterer.filter(log)
 
 
-def test_filter_below_min_level():
+def test_filter_below_min_level(levels):
     '''Test log with level below min does not pass.'''
     log = Log(level='info')
-    filterer = Level(min='warning')
+    filterer = Level(min='warning', levels=levels)
     assert filterer.filter(log) is True
 
 
-def test_filter_above_max_level():
+def test_filter_above_max_level(levels):
     '''Test log with level above max does not pass.'''
     log = Log(level='error')
-    filterer = Level(max='warning')
+    filterer = Level(max='warning', levels=levels)
     assert filterer.filter(log) is True
 
 
-def test_against_limitless_range():
+def test_against_limitless_range(levels):
     '''Test log passes against a limitless range.'''
     log = Log(level='info')
-    filterer = Level(min=None, max=None)
+    filterer = Level(min=None, max=None, levels=levels)
     assert filterer.filter(log) is False
 
