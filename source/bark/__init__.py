181a3aedff78f46beec703cb610a5ac6d3339f93
Martin Pengelly-Phillips <dev@thesociable.net>
2013-03-30 09:50:57 +0000

    Set Distribute handler as default top level handler.

--- source/bark/__init__.py
+++ source/bark/__init__.py
@@ -1,3 +1,9 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2013 Martin Pengelly-Phillips
 # :license: See LICENSE.txt.
+
+from .handler.distribute import Distribute
+
+#: Top level handler responsible for relaying all logs to other handlers.
+handle = Distribute()
+
