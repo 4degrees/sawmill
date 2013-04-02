694527823494d5050fe14129b819de8c76521dd7
Martin Pengelly-Phillips <dev@thesociable.net>
2013-04-02 10:01:51 +0100

    Add Template formatter and accompanying unit tests.

---
+++ source/bark/formatter/template.py
@@ -0,0 +1,58 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2013 Martin Pengelly-Phillips
+# :license: See LICENSE.txt.
+
+import string
+
+from .base import Formatter
+
+
+class Template(Formatter, string.Formatter):
+    '''Format :py:class:`~bark.log.Log` to string according to a template.'''
+
+    IGNORE, ERROR = ('ignore', 'error')
+
+    def __init__(self, template, mode=IGNORE):
+        '''Initialise formatter with *template*.
+
+        *mode* determines how to handle a missing key when formatting a log.
+        The default IGNORE will substitute an empty string for the missing
+        value. An alternative is ERROR, which would cause an error to be
+        raised.
+
+        '''
+        super(Template, self).__init__()
+        self.template = template
+        self.mode = mode
+
+    def format(self, log):
+        '''Return formatted data representing *log*.'''
+        return self.vformat(self.template, (), dict(**log))
+
+    def get_field(self, field_name, args, kwargs):
+        '''Convert and return *field_name* to an object to be formatted.
+
+        .. note::
+
+            Based on Python's string.Formatter.get_field source.
+
+        '''
+        first, rest = field_name._formatter_field_name_split()
+
+        try:
+            obj = self.get_value(first, args, kwargs)
+
+            for is_attr, index in rest:
+                if is_attr:
+                    obj = getattr(obj, index)
+                else:
+                    obj = obj[index]
+
+        except Exception:
+            if self.mode == self.IGNORE:
+                return '', first
+            else:
+                raise
+
+        return obj, first
+
---
+++ test/unit/formatter/test_template.py
@@ -0,0 +1,43 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2013 Martin Pengelly-Phillips
+# :license: See LICENSE.txt.
+
+import pytest
+
+from bark.log import Log
+from bark.formatter.template import Template
+
+
+def test_format():
+    '''Test formatted log result is as expected.'''
+    log = Log(message='A message', level='info')
+    template = Template('{level}:{message}')
+    assert template.format(log) == 'info:A message'
+
+
+def test_no_error_with_missing_values():
+    '''Test missing values replaced correctly when mode is IGNORE.'''
+    log = Log(message='A message')
+    template = Template('{level}:{message}', mode=Template.IGNORE)
+    assert template.format(log) == ':A message'
+
+
+def test_error_with_missing_values():
+    '''Test missing values cause KeyError when mode is ERROR.'''
+    log = Log(message='A message', data=[1, 2])
+
+    # Missing key
+    template = Template('{level}:{message}', mode=Template.ERROR)
+    with pytest.raises(KeyError):
+        template.format(log)
+
+    # Invalid attribute
+    template = Template('{message.type}', mode=Template.ERROR)
+    with pytest.raises(AttributeError):
+        template.format(log)
+
+    # Invalid index
+    template = Template('{data[3]}', mode=Template.ERROR)
+    with pytest.raises(IndexError):
+        template.format(log)
+
